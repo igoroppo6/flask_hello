03440147cae145bb43cc4365646f7b8e972f6f6c
igor balyakin <igoroppo6@gmail.com>
2017-03-22 17:04:32 +0300

    some commit

---
+++ app.py
@@ -0,0 +1,16 @@
+# -------- Flask ------
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/hello")
+
+
+def hello_world():
+    return "Hello, World\n"
+
+
+if __name__ == "__main__":
+    app.run()
